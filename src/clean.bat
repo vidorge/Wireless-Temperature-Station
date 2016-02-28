76aba3f40c42e2f5204efcec4f799867cca77b5c
Vidor Gencel <vidorge@gmail.com>
2016-02-28 16:14:56 +0100

    Clean script

---
+++ src/clean.bat
@@ -0,0 +1,17 @@
+del *.cp
+del *.asm
+del *.mil
+del *.emcl
+del *.bin
+del *.ini
+del *.bmk
+del *.brk
+del *.dbg
+del *.dct
+del *.dlt
+del *.log
+del *.dic
+del *.lst
+del *.mcpar_callertable.txt
+del *.ini
+
